48a9d6b2bd193af7c2ac2377d68e4ebba1776580
developbyarya <aryain3rd@gmail.com>
2021-07-21 11:02:36 +0700

    update 2.0, add requirement

--- gApiSystem/settings.py
+++ gApiSystem/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'personal_site.apps.PersonalSiteConfig'
+    'personal_site.apps.PersonalSiteConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
